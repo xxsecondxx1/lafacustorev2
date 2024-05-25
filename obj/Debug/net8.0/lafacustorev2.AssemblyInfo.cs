485e27a10c0ec881544849e7b8e6b667358fb0ec
xxsecondxx1 <second_u2@hotmail.com>
2024-05-24 19:54:13 -0500

    mejorando_diseño

--- obj/Debug/net8.0/lafacustorev2.AssemblyInfo.cs
+++ obj/Debug/net8.0/lafacustorev2.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lafacustorev2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+efa7fd427557784834fbe36cbc1f883c68b4a886")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fad6e31cc3ac969ea15d66ff8f434043cc4f3cbf")]
 [assembly: System.Reflection.AssemblyProductAttribute("lafacustorev2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lafacustorev2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
